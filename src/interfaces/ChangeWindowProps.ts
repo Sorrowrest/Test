6fb5a7dc2c639f9e9b7871c1463ae7b25c4bbff5
Anastasia <61600483+Sorrowrest@users.noreply.github.com>
2021-08-14 17:22:49 +0300

    Delete ChangeWindowProps.ts

--- src/interfaces/ChangeWindowProps.ts
+++
@@ -1,8 +0,0 @@
-import { ElementCreated } from './ElementCreated';
-import { ElementParams } from './ElementParams';
-
-export interface ChangeWindowProps {
-    elementParams: ElementParams | undefined;
-    closeChangeWindow: () => void;
-    changeSomethingEl: (newElement: ElementParams) => void;
-}
